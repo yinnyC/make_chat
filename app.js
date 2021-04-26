b7844660f1ca0bb91ed3675039985504fec9549c
Yin <yinnnn17@gmail.com>
2021-04-25 18:38:37 -0700

    Users can log in

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const server = require('http').Server(app);
 //Socket.io
 const io = require('socket.io')(server);
 io.on("connection", (socket) => {
-  console.log("🔌 New user connected! 🔌");
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
 })
 
 const exphbs  = require('express-handlebars');
---
+++ sockets/chat.js
@@ -0,0 +1,11 @@
+// chat.js
+module.exports = (io, socket) => {
+
+  // Listen for "new user" socket emits
+  socket.on('new user', (username) => {
+    console.log(`${username} has joined the chat! ✋`);
+    //Send the username to all clients currently connected
+    io.emit("new user", username);
+  })
+
+}
